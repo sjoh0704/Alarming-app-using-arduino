ebbc8f26e9cb2667886ac89b69cfdbb3218e677c
SeungJu <sjoh0704@daum.net>
2020-11-13 14:37:19 +0900

    add video recording and functions

--- app/src/main/java/org/techtown/arduinoblue/MainActivity.java
+++ app/src/main/java/org/techtown/arduinoblue/MainActivity.java
@@ -1,8 +1,10 @@
 package org.techtown.arduinoblue;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
@@ -10,32 +12,71 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Camera;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SurfaceHolder.Callback {
     TextView mTvBluetoothStatus;
     TextView mTvReceiveData;
     TextView mTvSendData;
     Button mBtnBluetoothOn;
     Button mBtnBluetoothOff;
     Button mBtnConnect;
-    Button mBtnSendData;
+    private Camera camera;
+    private MediaRecorder mediaRecorder;
+    private Button btn_record, btn_upload;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private boolean recording = false;
+    private String filename = null;
+    private String dirPath;
+    private String TAG = "TAG";
+    private Button btn_send;
+    private EditText textPhoneNo;
+    private String phoneNum = null;
+    private  ProgressDialog progressDialog;
+
+    private GpsTracker gpsTracker;
 
 
     BluetoothAdapter mBluetoothAdapter;
@@ -57,6 +98,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        allowPermission();  //Ted permission으로 권한 얻어오기
+        makeDir();
+
+        btn_record = findViewById(R.id.btn_record);
+        btn_upload = findViewById(R.id.btn_upload);
+        btn_send = findViewById(R.id.btn_send);
+
+        btn_record.setOnClickListener(this);
+        btn_upload.setOnClickListener(this);
+        btn_send.setOnClickListener(this);
+        textPhoneNo = findViewById(R.id.editTextPhoneNumber);
+
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please waiting...");
+        progressDialog.setCancelable(false);
+        progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Horizontal);
+
+
+
 
         mTvBluetoothStatus = (TextView)findViewById(R.id.tvBluetoothStatus);
         mTvReceiveData = (TextView)findViewById(R.id.tvReceiveData);
@@ -64,7 +125,6 @@ public class MainActivity extends AppCompatActivity {
         mBtnBluetoothOn = (Button)findViewById(R.id.btnBluetoothOn);
         mBtnBluetoothOff = (Button)findViewById(R.id.btnBluetoothOff);
         mBtnConnect = (Button)findViewById(R.id.btnConnect);
-        mBtnSendData = (Button)findViewById(R.id.btnSendData);
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -87,15 +147,7 @@ public class MainActivity extends AppCompatActivity {
                 listPairedDevices();
             }
         });
-        mBtnSendData.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(mThreadConnectedBluetooth != null) {
-                    mThreadConnectedBluetooth.write(mTvSendData.getText().toString());
-                    mTvSendData.setText("");
-                }
-            }
-        });
+
 
         mBluetoothHandler = new Handler(){
             public void handleMessage(android.os.Message msg){
@@ -204,6 +256,204 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onClick(View view) {
+
+
+
+
+
+        switch(view.getId()) {
+
+            case R.id.btn_record:
+                if (recording) {
+
+                    mediaRecorder.stop();
+                    mediaRecorder.release();
+                    camera.lock();
+                    recording = false;
+
+                    Toast.makeText(MainActivity.this, "firebase에 업로드중 기다려주세요", Toast.LENGTH_SHORT).show();
+
+                    btn_upload.callOnClick();
+                    btn_send.callOnClick();
+
+
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if (textPhoneNo.length() == 11) {
+                                phoneNum = textPhoneNo.getText().toString();
+                                //과부화도 덜되고 동영상 처리는 여기서 하는게 좋다
+                                Toast.makeText(MainActivity.this, "녹화가 시작되었습니다.", Toast.LENGTH_SHORT).show();
+                                try {
+
+
+                                    mediaRecorder = new MediaRecorder();
+                                    camera.unlock();
+                                    mediaRecorder.setCamera(camera);
+                                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+                                    mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+                                    mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_480P));
+                                    mediaRecorder.setOrientationHint(90);
+
+                                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                    Date now = new Date();
+                                    filename = formatter.format(now) + ".mp4";
+
+
+                                    mediaRecorder.setOutputFile(dirPath + "/" + filename);
+                                    mediaRecorder.setPreviewDisplay(surfaceHolder.getSurface());
+                                    mediaRecorder.prepare();
+                                    mediaRecorder.start();
+                                    recording = true;
+
+
+                                    MediaScanner ms = MediaScanner.newInstance(MainActivity.this);
+                                    try {
+                                        ms.mediaScanning(dirPath + "/" + filename);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                        Log.d("MediaScan", "ERROR" + e);
+                                    } finally {
+                                    }
+//                                galleryAddPic();
+
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    mediaRecorder.release();
+                                }
+                            } else {
+                                Toast.makeText(MainActivity.this, "전화번호를 입력해주세요", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+
+                break;
+            case R.id.btn_upload:
+                if (filename != null) {
+
+                    Log.e("파일명 확인: ", filename);
+
+                    FirebaseStorage storage = FirebaseStorage.getInstance();
+
+                    Uri file = Uri.fromFile(new File(dirPath + "/" + filename));
+                    StorageReference storageRef = storage.getReferenceFromUrl("gs://arduinovideo-5c84c.appspot.com/").child(dirPath + "/" + filename);
+                    //storage url 적는란
+
+
+                    Log.e("URi 확인: ", String.valueOf(file));
+                    storageRef.putFile(file)
+                            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                                    double progress = (100.0 * taskSnapshot.getBytesTransferred());
+                                    progressDialog.show();
+                                    if (progress >= 100)
+                                        progressDialog.dismiss();
+                                }
+                            })
+
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    Toast.makeText(MainActivity.this, "업로드 성공", Toast.LENGTH_SHORT).show();
+                                    filename = null;
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(MainActivity.this, "업로드 실패", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+
+                } else {
+                    Toast.makeText(MainActivity.this, "파일 없음", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+
+            case R.id.btn_send:
+                gpsTracker = new GpsTracker(this);
+                double latitude = gpsTracker.getLatitude();
+                double longtitude = gpsTracker.getLongitude();
+                String address = getCurrentAddress(latitude, longtitude);
+                String location = "\n위도: " + latitude + "\n경도: " + longtitude;
+                sendSms(phoneNum, address + location);
+                phoneNum = null;
+                Toast.makeText(this, "문자 메시지 전송", Toast.LENGTH_SHORT).show();
+
+                break;
+
+
+        }}
+
+    private void sendSms(String phoneNum, String msg) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNum, null, msg, null, null);
+
+
+    }
+
+
+    public String getCurrentAddress( double latitude, double longitude) {
+
+        //지오코더... GPS를 주소로 변환
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses;
+
+        try {
+
+            addresses = geocoder.getFromLocation(
+                    latitude,
+                    longitude,
+                    7);
+        } catch (IOException ioException) {
+            //네트워크 문제
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+
+        }
+
+
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+
+        }
+
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString()+"\n";
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
     private class ConnectedBluetoothThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final InputStream mmInStream;
@@ -242,20 +492,84 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        public void write(String str) {
-            byte[] bytes = str.getBytes();
-            try {
-                mmOutStream.write(bytes);
-            } catch (IOException e) {
-                Toast.makeText(getApplicationContext(), "데이터 전송 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
-            }
-        }
-        public void cancel() {
-            try {
-                mmSocket.close();
-            } catch (IOException e) {
-                Toast.makeText(getApplicationContext(), "소켓 해제 중 오류가 발생했습니다.", Toast.LENGTH_LONG).show();
-            }
-        }
+
+
     }
+
+    private void makeDir() {
+        String str = Environment.getExternalStorageState();
+        if ( str.equals(Environment.MEDIA_MOUNTED)) {
+
+            dirPath = "/sdcard/DCIM/BlackBox";
+            File file = new File(dirPath);
+            if( !file.exists() )  // 원하는 경로에 폴더가 있는지 확인
+            {
+                Log.e("TAG : ", "디렉토리 생성");
+                file.mkdirs();
+            }
+            else Log.e("TAG : ", "디렉토리 이미존재");
+
+        }
+        else
+            Toast.makeText(this, "SD Card 인식 실패", Toast.LENGTH_SHORT).show();
+    }
+    PermissionListener permission = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            camera = Camera.open();
+            camera.setDisplayOrientation(90);
+            surfaceView = findViewById(R.id.surfaceView);
+            surfaceHolder = surfaceView.getHolder();
+            surfaceHolder.addCallback(MainActivity.this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+
+        }
+    };
+
+    private void allowPermission() {
+
+        TedPermission.with(this)
+                .setPermissionListener(permission)
+                .setRationaleMessage("필요한 권한을 허용해주세요.")
+                .setDeniedMessage("권한이 거부되었습니다.")
+                .setPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO, Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION )
+                .check();
+    }
+
+    private void refreshCamera(Camera camera) {
+        if(surfaceHolder.getSurface() == null){
+            return ;
+        }
+        try {
+            camera.stopPreview();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        setCamera(camera);
+
+
+    }
+
+    private void setCamera(Camera cam) {
+        camera = cam;
+
+    }
+
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+
+
+
 }
